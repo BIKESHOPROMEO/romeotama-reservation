d00d535b369d272eaa4e5b651047f2a81aeefb96
BIKESHOPROMEO <tama@realeyes.co.jp>
2025-08-25 19:51:48 +0900

    Add files via upload

--- pages/api/calendar-availability.js
+++ pages/api/calendar-availability.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const GAS_URL = "https://script.google.com/macros/s/あなたのGASデプロイURL/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbwvqxdEp4sWhAACzZRlPe9LzNdNxg2lY5XvIh_uRcfWJHMTnKlFaetKAdwSPdiGzTtwDg/exec";
 
   if (req.method !== "GET") {
     return res.status(405).json({ message: "Method Not Allowed" });
